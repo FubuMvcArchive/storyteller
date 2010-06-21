132359e8174b3a7ea568d2682ca30013e9a981cd
Jeremy Miller <jeremydmiller@yahoo.com>
2010-06-20 20:39:46 -0500

    added the InstallGems.bat file

---
+++ InstallGems.bat
@@ -0,0 +1,8 @@
+@ECHO *** Installing Rake
+@call gem install rake --no-rdoc --no-ri
+
+@ECHO *** Installing RubyZip
+@call gem install rubyzip --no-rdoc --no-ri
+
+@ECHO *** Installing Albacore (build support tools)
+@call gem install albacore --no-rdoc --no-ri
