e8b37f356f57e1e48e07a5f71adf89b1f83479c9
Jeremy Miller <jeremydmiller@yahoo.com>
2010-07-29 11:59:35 -0500

    cleaning up test editor html

--- source/StoryTeller.UserInterface/Editing/HTML/TestEditorTag.cs
+++ source/StoryTeller.UserInterface/Editing/HTML/TestEditorTag.cs
@@ -1,26 +1,56 @@
 using HtmlTags;
 using StoryTeller.Html;
 using StoryTeller.Model;
+using System.Linq;
 
 namespace StoryTeller.UserInterface.Editing.HTML
 {
-    public class TestEditorTag : TestHolderTag
+    
+
+    public class TestEditorTag : HtmlTag
     {
-        public TestEditorTag(FixtureLibrary library)
+        private readonly HtmlTag _container;
+
+        public TestEditorTag(FixtureLibrary library) : base("div")
         {
+            AddClass("main");
+            Add("h2").AddClass(HtmlClasses.TEST_NAME);
+            Id("testEditor");
+
+            AddClass(HtmlClasses.TEST_EDITOR);
+
+            _container = Add("div").AddClasses("container", HtmlClasses.SECTION, "test-editor");
+
             FixtureGraph fixture = library.BuildTopLevelGraph();
 
-            HtmlTag selector = new GrammarSelector(fixture).Build();
+            HtmlTag holder = new HolderTag(fixture).AddClass("top-level-holder");
+            holder.Children.Last().Visible(false);
 
             Container
                 .MetaData(GrammarConstants.LEAF_NAME, GrammarConstants.TEST)
                 .MetaData(GrammarConstants.FIXTURE, GrammarConstants.TEST)
                 .MetaData(GrammarConstants.SELECTION_MODE, SelectionMode.OneOrMore.ToString())
-                .Child(new HolderTag(fixture).AddClass("top-level-holder"))
-                .Child(new HtmlTag("hr"))
-                .Child(selector);
+                .Child(holder)
+                .Child(new HtmlTag("hr"));
 
+            
+            Container.ActionLink(fixture.Policies.AddGrammarText, GrammarConstants.ADD_SECTION_ACTIVATOR);
+            Container.Child(new GrammarSelector(fixture).Build());
 
+                
         }
+
+        /*
+            Add("div").AddClass(GrammarConstants.STEP_HOLDER);
+
+            if (fixture.IsSingleSelection() || fixture.Policies.SelectionMode == SelectionMode.MandatoryAutoSelect)
+            {
+                return;
+            }
+
+            this.ActionLink(fixture.Policies.AddGrammarText, GrammarConstants.ADD_SECTION_ACTIVATOR);
+         */
+
+        public HtmlTag Container { get { return _container; } }
     }
 }
