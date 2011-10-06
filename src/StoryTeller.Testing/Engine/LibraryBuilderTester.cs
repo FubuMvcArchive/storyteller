76f4eaf8003af4c7f2eb27933cddc2ee253a2234
Jeremy Miller <jeremydmiller@yahoo.com>
2011-10-06 09:34:14 -0500

    initial fubu-ize/ripple-ize conversion

---
+++ src/StoryTeller.Testing/Engine/LibraryBuilderTester.cs
@@ -0,0 +1,300 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using FubuCore.Util;
+using NUnit.Framework;
+using Rhino.Mocks;
+using StoryTeller.Engine;
+using StoryTeller.Model;
+using StoryTeller.Samples;
+using TestContext = StoryTeller.Engine.TestContext;
+
+namespace StoryTeller.Testing.Engine
+{
+    [TestFixture]
+    public class when_building_a_library_with_a_non_inclusive_filter
+    {
+        #region Setup/Teardown
+
+        [SetUp]
+        public void SetUp()
+        {
+            observer = MockRepository.GenerateMock<IFixtureObserver>();
+            var filter = new CompositeFilter<Type>();
+            filter.Includes += t => t.Name.StartsWith("M");
+
+
+            builder = new LibraryBuilder(observer, filter);
+
+            library = builder.Build(new TestContext(x => x.AddFixturesFromThisAssembly()));
+        }
+
+        #endregion
+
+        private LibraryBuilder builder;
+        private IFixtureObserver observer;
+        private FixtureLibrary library;
+
+        [Test]
+        public void should_contain_fixtures_that_match_the_filter()
+        {
+            library.HasFixture("Missouri").ShouldBeTrue();
+            library.HasFixture("Michigan").ShouldBeTrue();
+            library.HasFixture("Montana").ShouldBeTrue();
+        }
+
+        [Test]
+        public void should_exclude_fixtures_present_in_the_container_that_do_not_match_the_filter()
+        {
+            library.HasFixture("Arkansas").ShouldBeFalse();
+        }
+
+        [Test]
+        public void still_has_dtos_for_each_possible_fixture()
+        {
+            var allNames = library.AllFixtures.OrderBy(x => x.Name).Select(x => x.Name).ToList();
+            allNames.ShouldContain("Arkansas");
+            allNames.ShouldContain("Michigan");
+            allNames.ShouldContain("Missouri");
+            allNames.ShouldContain("Montana");
+        }
+
+        [Test]
+        public void fixture_dto_has_all_the_properties()
+        {
+            var dto = library.AllFixtures.FirstOrDefault(x => x.Name == "Arkansas");
+            dto.Namespace.ShouldEqual(typeof (ArkansasFixture).Namespace);
+            dto.Fullname.ShouldEqual(typeof (ArkansasFixture).FullName);
+        }
+    }
+
+    public class MissouriFixture : Fixture{}
+    public class ArkansasFixture : Fixture{}
+    public class MontanaFixture : Fixture{}
+    public class MichiganFixture : Fixture{}
+
+
+    [TestFixture]
+    public class LibraryBuilderTester
+    {
+        #region Setup/Teardown
+
+        [SetUp]
+        public void SetUp()
+        {
+            observer = MockRepository.GenerateMock<IFixtureObserver>();
+            builder = new LibraryBuilder(observer, new CompositeFilter<Type>());
+
+            builder.FixtureCount = 23;
+        }
+
+        #endregion
+
+        private LibraryBuilder builder;
+        private IFixtureObserver observer;
+
+        [Test]
+        public void read_a_fixture_failure()
+        {
+            var exception = new NotImplementedException();
+            builder.LogFixtureFailure("bad fixture", exception);
+
+            FixtureGraph fixture = builder.Library.FixtureFor("bad fixture");
+
+            fixture.AllErrors().Count().ShouldEqual(1);
+
+            GrammarError error = fixture.AllErrors().First();
+            error.Message.ShouldEqual("Fixture 'bad fixture' could not be loaded");
+            error.ErrorText.ShouldEqual(exception.ToString());
+        }
+
+        [Test]
+        public void read_a_fixture_will_record_the_Title_of_the_fixture_if_it_is_explicitly_set()
+        {
+            var fixture = new GrammarErrorFixture
+            {
+                Title = "the bad grammars"
+            };
+
+            builder.ReadFixture("GrammarError", fixture);
+
+            builder.Library.FixtureFor("GrammarError").Label.ShouldEqual("the bad grammars");
+        }
+
+        [Test]
+        public void read_a_fixture_will_use_the_fixture_name_for_the_title_if_the_fixture_title_is_empty()
+        {
+            var fixture = new GrammarErrorFixture();
+
+
+            builder.ReadFixture("GrammarError", fixture);
+
+            builder.Library.FixtureFor("GrammarError").Label.ShouldEqual("GrammarError");
+        }
+
+        [Test]
+        public void read_a_fixture_with_grammar_errors()
+        {
+            var fixture = new GrammarErrorFixture();
+            fixture.Errors.Count().ShouldEqual(2);
+
+            builder.ReadFixture("GrammarError", fixture);
+
+            FixtureGraph fixtureGraph = builder.Library.FixtureFor("GrammarError");
+            fixtureGraph.AllErrors().Count().ShouldEqual(2);
+
+            fixtureGraph.AllErrors().Each(x => x.Node.ShouldEqual(fixtureGraph));
+        }
+
+        [Test]
+        public void reading_a_fixture_failure_will_send_a_reading_fixture_message()
+        {
+            var exception = new NotImplementedException();
+            builder.LogFixtureFailure("bad fixture", exception);
+
+            observer.AssertWasCalled(x => x.ReadingFixture(23, 1, "bad fixture"));
+        }
+
+        [Test]
+        public void reading_a_fixture_will_send_a_message_to_the_observer()
+        {
+            builder.ReadFixture("Good", new StubFixture());
+
+            observer.AssertWasCalled(x => x.ReadingFixture(23, 1, "Good"));
+
+            builder.ReadFixture("Good2", new StubFixture());
+
+            observer.AssertWasCalled(x => x.ReadingFixture(23, 2, "Good2"));
+        }
+    }
+
+
+    [TestFixture]
+    public class creating_policies_for_fixtures
+    {
+        #region Setup/Teardown
+
+        [SetUp]
+        public void SetUp()
+        {
+            var context = new TestContext(x => { x.AddFixture<FixtureWithHiddenGrammarsFixture>(); });
+
+            var observer = MockRepository.GenerateMock<IFixtureObserver>();
+            var builder = new LibraryBuilder(observer, new CompositeFilter<Type>());
+            builder.Build(context);
+
+            library = builder.Library;
+        }
+
+        #endregion
+
+        private FixtureLibrary library;
+
+        [Test]
+        public void fixture_graph_should_have_the_policies_from_the_original_fixture()
+        {
+            FixtureGraph fixture = library.FixtureFor(typeof (FixtureWithHiddenGrammarsFixture).GetFixtureAlias());
+
+            fixture.Policies.IsHidden("Hidden1").ShouldBeTrue();
+            fixture.Policies.IsHidden("Hidden2").ShouldBeTrue();
+            fixture.Policies.IsHidden("NotHidden1").ShouldBeFalse();
+            fixture.Policies.IsHidden("NotHidden2").ShouldBeFalse();
+        }
+
+        [Test]
+        public void load_a_fixture_with_fixture_level_tags()
+        {
+            var fixture = new TagsFixture();
+            fixture.Policies.Tags().ShouldHaveTheSameElementsAs("d", "e");
+        }
+
+        [Test]
+        public void load_a_fixture_with_grammar_level_tags()
+        {
+            var fixture = new TagsFixture();
+            fixture.Policies.Tags("Go1").Count().ShouldEqual(0);
+            fixture.Policies.Tags("Go2").ShouldHaveTheSameElementsAs("a", "b");
+            fixture.Policies.Tags("Go3").ShouldHaveTheSameElementsAs("c");
+        }
+    }
+
+    [TestFixture]
+    public class when_reading_startup_actions_into_the_library
+    {
+        private FixtureLibrary library;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var builder = new LibraryBuilder(new NulloFixtureObserver(), new CompositeFilter<Type>());
+            var registry = new FixtureRegistry();
+            registry.AddFixturesFromAssemblyContaining<SetUserAction>();
+
+            var container = registry.BuildContainer();
+            var context = new TestContext(container);
+
+
+            library = builder.Build(context);
+        }
+
+        [Test]
+        public void library_startup_action_names_should_include_the_names_of_the_registered_startup_actions()
+        {
+            library.StartupActions.ShouldHaveTheSameElementsAs("SetUser", "StartWebApp");
+        }
+    }
+
+    [Tag("d", "e")]
+    public class TagsFixture : Fixture
+    {
+        public void Go1()
+        {
+        }
+
+        [Tag("a", "b")]
+        public void Go2()
+        {
+        }
+
+        [Tag("c")]
+        public void Go3()
+        {
+        }
+    }
+
+    public class FixtureWithHiddenGrammarsFixture : Fixture
+    {
+        [Hidden]
+        public void Hidden1()
+        {
+        }
+
+        public void NotHidden1()
+        {
+        }
+
+        [Hidden]
+        public IGrammar Hidden2()
+        {
+            return Paragraph("something", x => { });
+        }
+
+        public IGrammar NotHidden2()
+        {
+            return Paragraph("something", x => { });
+        }
+    }
+
+    public class GrammarErrorFixture : Fixture
+    {
+        public IGrammar Bad1()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IGrammar Bad2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/StoryTeller.Testing/Engine/TestRunnerTester.cs
@@ -0,0 +1,517 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using NUnit.Framework;
+using Rhino.Mocks;
+using StoryTeller.Assertions;
+using StoryTeller.Domain;
+using StoryTeller.Engine;
+using StoryTeller.Engine.Constraints;
+using StoryTeller.Execution;
+using StoryTeller.Model;
+using System.Linq;
+using StructureMap;
+using TestContext = StoryTeller.Engine.TestContext;
+
+namespace StoryTeller.Testing.Engine
+{
+
+    public class RecordingSystem : ISystem
+    {
+        private readonly List<string> _messages = new List<string>();
+
+        public void Record(string message)
+        {
+            _messages.Add(message);
+        }
+
+        public string[] Messages
+        {
+            get
+            {
+                return _messages.ToArray();
+            }
+        }
+
+        public void Clear()
+        {
+            _messages.Clear();
+        }
+
+        public object Get(Type type)
+        {
+            var container = new Container(x => x.For<RecordingSystem>().Use(this));
+            return container.GetInstance(type);
+        }
+
+        public void RegisterServices(ITestContext context)
+        {
+            Record("RegisterServices");
+            context.Store(this);
+        }
+
+        public void SetupEnvironment()
+        {
+            Record("SetupEnvironment");
+        }
+
+        public void TeardownEnvironment()
+        {
+            Record("TeardownEnvironment");
+        }
+
+        public void Setup()
+        {
+            Record("Setup");
+        }
+
+        public void Teardown()
+        {
+            Record("Teardown");
+        }
+
+        public void RegisterFixtures(FixtureRegistry registry)
+        {
+            
+        }
+    }
+
+    public class Startup1Action : IStartupAction
+    {
+        private readonly RecordingSystem _system;
+
+        public Startup1Action(RecordingSystem system)
+        {
+            _system = system;
+        }
+
+        public void Startup(ITestContext context)
+        {
+           _system.Record("Setup 1");
+        }
+
+        public void Teardown(ITestContext context)
+        {
+            _system.Record("Teardown 1");
+        }
+    }
+
+    public class Startup2Action : IStartupAction
+    {
+        private readonly RecordingSystem _system;
+
+        public Startup2Action(RecordingSystem system)
+        {
+            _system = system;
+        }
+
+        public void Startup(ITestContext context)
+        {
+            _system.Record("Setup 2");
+        }
+
+        public void Teardown(ITestContext context)
+        {
+            _system.Record("Teardown 2");
+        }
+    }
+
+    public class RecordingFixture : Fixture
+    {
+        private readonly RecordingSystem _system;
+
+        public RecordingFixture(RecordingSystem system)
+        {
+            _system = system;
+        }
+
+        public void Execute()
+        {
+            _system.Record("Execute");
+        }        
+    }
+
+    [TestFixture]
+    public class when_executing_a_test
+    {
+        private RecordingSystem system;
+        private TestRunner runner;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var library = FixtureLibrary.For(x => x.AddFixture<RecordingFixture>());
+            system = new RecordingSystem();
+            var fixtureContainerSource = new FixtureContainerSource(new Container(x =>
+            {
+                x.For<IFixture>().Add<RecordingFixture>().Named("Recording");
+                x.For<IStartupAction>().Add<Startup1Action>().Named("Startup1");
+                x.For<IStartupAction>().Add<Startup2Action>().Named("Startup2");
+            }));
+            fixtureContainerSource.RegisterFixture("Recording", typeof (RecordingFixture));
+
+            runner = new TestRunner(system, library, fixtureContainerSource);
+
+            var test = new Test("something");
+            test.Add(new Section("Recording").WithStep("Execute"));
+
+            runner.RunTest(new TestExecutionRequest()
+            {
+                Test = test,
+                TimeoutInSeconds = 1200,
+                StartupActions = new string[]{"Startup1", "Startup2"}
+            });
+
+        }
+
+        [Test]
+        public void should_call_setup_on_both_startup_actions()
+        {
+            system.Messages.ShouldContain("Setup 1");
+            system.Messages.ShouldContain("Setup 2");
+        }
+
+        [Test]
+        public void should_call_teardown_on_both_startup_actions()
+        {
+            system.Messages.ShouldContain("Teardown 1");
+            system.Messages.ShouldContain("Teardown 2");
+        }
+
+        [Test]
+        public void should_start_the_application_because_it_has_not_already_been_started()
+        {
+            system.Messages.ShouldContain("SetupEnvironment");
+        }
+
+        [Test]
+        public void should_register_services()
+        {
+            system.Messages.ShouldContain("RegisterServices");
+        }
+
+        [Test]
+        public void should_setup_the_execution()
+        {
+            system.Messages.ShouldContain("Setup");
+        }
+
+        [Test]
+        public void should_have_executed_the_fixture()
+        {
+            system.Messages.ShouldContain("Execute");
+        }
+
+        [Test]
+        public void should_teardown_after_the_test()
+        {
+            system.Messages.ShouldContain("Teardown");
+        }
+
+        [Test]
+        public void should_not_teardown_the_environment()
+        {
+            system.Messages.ShouldNotContain("TeardownEnvironment");
+        }
+
+        [Test]
+        public void should_do_the_steps_in_the_proper_order()
+        {
+            system.Messages.Length.ShouldEqual(9);
+
+            system.Messages[0].ShouldEqual("SetupEnvironment");
+            system.Messages[1].ShouldEqual("RegisterServices");
+            system.Messages[2].ShouldEqual("Setup");
+            system.Messages[3].ShouldEqual("Setup 1");
+            system.Messages[4].ShouldEqual("Setup 2");
+
+
+            system.Messages[5].ShouldEqual("Execute");
+            system.Messages[6].ShouldEqual("Teardown 1");
+            system.Messages[7].ShouldEqual("Teardown 2");
+
+            system.Messages[8].ShouldEqual("Teardown");
+        }
+    }
+
+    [TestFixture]
+    public class starting_the_application_only_happens_on_the_first_Test_if_necessary
+    {
+        private RecordingSystem system;
+        private TestRunner runner;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var library = FixtureLibrary.For(x => x.AddFixture<RecordingFixture>());
+            system = new RecordingSystem();
+            var fixtureContainerSource = new FixtureContainerSource(new Container(x =>
+            {
+                x.For<IFixture>().Add<RecordingFixture>().Named("Recording");
+            }));
+            fixtureContainerSource.RegisterFixture("Recording", typeof(RecordingFixture));
+
+            runner = new TestRunner(system, library, fixtureContainerSource);
+
+            var test = new Test("something");
+            test.Add(new Section("Recording").WithStep("Execute"));
+
+            runner.RunTest(new TestExecutionRequest()
+            {
+                Test = test,
+                TimeoutInSeconds = 1200
+            });
+
+            runner.RunTest(new TestExecutionRequest()
+            {
+                Test = test,
+                TimeoutInSeconds = 1200
+            });
+
+        }
+
+        [Test]
+        public void setup_environment_should_only_be_called_once()
+        {
+            system.Messages[0].ShouldEqual("SetupEnvironment");
+            system.Messages.Count(x => x == "SetupEnvironment").ShouldEqual(1);
+        }
+    }
+
+    [TestFixture]
+    public class when_executing_a_test_after_the_application_has_been_started
+    {
+        private RecordingSystem system;
+        private TestRunner runner;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var library = FixtureLibrary.For(x => x.AddFixture<RecordingFixture>());
+            system = new RecordingSystem();
+
+
+            var fixtureContainerSource = new FixtureContainerSource(new Container(x =>
+            {
+                x.For<IFixture>().Add<RecordingFixture>().Named("Recording");
+            }));
+            fixtureContainerSource.RegisterFixture("Recording", typeof(RecordingFixture));
+
+            
+
+            runner = new TestRunner(system, library, fixtureContainerSource);
+            runner.Lifecycle.StartApplication();
+
+            var test = new Test("something");
+            test.Add(new Section("Recording").WithStep("Execute"));
+
+            runner.RunTest(new TestExecutionRequest()
+            {
+                Test = test,
+                TimeoutInSeconds = 1200
+            });
+
+        }
+
+        [Test]
+        public void should_do_the_steps_in_the_proper_order_and_not_repeat_SetupEnvironment()
+        {
+            system.Messages.Length.ShouldEqual(5);
+
+            system.Messages[0].ShouldEqual("SetupEnvironment");
+            system.Messages[1].ShouldEqual("RegisterServices");
+            system.Messages[2].ShouldEqual("Setup");
+            system.Messages[3].ShouldEqual("Execute");
+            system.Messages[4].ShouldEqual("Teardown");
+        }
+    }
+
+    [TestFixture]
+    public class when_recycling_the_test_runner_environment
+    {
+        private RecordingSystem system;
+        private TestRunner runner;
+        private SystemLifecycle lifecycle;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var library = FixtureLibrary.For(x => x.AddFixture<RecordingFixture>());
+            system = new RecordingSystem();
+
+
+            var fixtureContainerSource = new FixtureContainerSource(new Container(x =>
+            {
+                x.For<IFixture>().Add<RecordingFixture>().Named("Recording");
+            }));
+            fixtureContainerSource.RegisterFixture("Recording", typeof(RecordingFixture));
+
+            lifecycle = new SystemLifecycle(system);
+            runner = new TestRunner(lifecycle, library, fixtureContainerSource);
+            lifecycle.StartApplication();
+
+            lifecycle.RecycleEnvironment();
+
+            var test = new Test("something");
+            test.Add(new Section("Recording").WithStep("Execute"));
+
+            runner.RunTest(new TestExecutionRequest()
+            {
+                Test = test,
+                TimeoutInSeconds = 1200
+            });
+
+            system.Messages.Each(x => Debug.WriteLine(x));
+        }
+
+        [Test]
+        public void should_do_the_steps_in_the_proper_order_and_not_repeat_SetupEnvironment()
+        {
+            system.Messages.Length.ShouldEqual(7);
+
+            system.Messages[0].ShouldEqual("SetupEnvironment");
+            system.Messages[1].ShouldEqual("TeardownEnvironment");
+            system.Messages[2].ShouldEqual("SetupEnvironment");
+
+            system.Messages[3].ShouldEqual("RegisterServices");
+            system.Messages[4].ShouldEqual("Setup");
+            system.Messages[5].ShouldEqual("Execute");
+            system.Messages[6].ShouldEqual("Teardown");
+        }
+    }
+
+    [TestFixture]
+    public class when_disposing_a_test_runner
+    {
+        private RecordingSystem system;
+        private TestRunner runner;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var library = FixtureLibrary.For(x => x.AddFixture<RecordingFixture>());
+            system = new RecordingSystem();
+
+
+            var fixtureContainerSource = new FixtureContainerSource(new Container(x =>
+            {
+                x.For<IFixture>().Add<RecordingFixture>().Named("Recording");
+            }));
+            fixtureContainerSource.RegisterFixture("Recording", typeof(RecordingFixture));
+
+            runner = new TestRunner(system, library, fixtureContainerSource);
+
+            runner.Dispose();
+
+
+        }
+
+        [Test]
+        public void should_teardown_the_environment()
+        {
+            system.Messages.ShouldHaveTheSameElementsAs("TeardownEnvironment");
+        }
+    }
+    
+
+    [TestFixture]
+    public class TestRunnerTester : AAAMockingContext<TestContext>
+    {
+
+
+        [Test]
+        public void run_a_test_when_setup_blows_up_do_not_rethrow_exception_and_log_the_exception_to_the_test()
+        {
+            var runner = TestRunnerBuilder.ForSystem<SystemThatBlowsUpInSetup>();
+
+            var test = new Test("Some test");
+
+            runner.RunTest(test);
+
+            test.LastResult.ExceptionText.ShouldContain("NotImplementedException");
+        }
+
+        [Test]
+        public void run_a_test_when_teardown_blows_up_do_not_rethrow_exception_and_log_the_exception_to_the_test()
+        {
+            var system = MockRepository.GenerateMock<ISystem>();
+            var fixtureContainerSource = new FixtureContainerSource(new Container(x =>
+            {
+                x.For<IFixture>().Add<RecordingFixture>().Named("Recording");
+            }));
+            var runner = new TestRunner(system, new FixtureLibrary(), fixtureContainerSource);
+
+            system.Expect(x => x.Teardown()).Throw(new NotImplementedException());
+
+            var test = new Test("something");
+            runner.RunTest(test);
+
+            test.LastResult.ExceptionText.ShouldContain("NotImplementedException");
+
+
+        }
+    }
+
+
+    public class SystemThatBlowsUpInSetup : ISystem
+    {
+        public T Get<T>() where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Get(Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterServices(ITestContext context)
+        {
+        }
+
+        public void SetupEnvironment()
+        {
+        }
+
+        public void TeardownEnvironment()
+        {
+        }
+
+        public void Setup()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Teardown()
+        {
+        }
+
+        public void RegisterFixtures(FixtureRegistry registry)
+        {
+            
+        }
+    }
+
+
+
+
+    public class GrammarThatAssertsFailure : IGrammar
+    {
+        #region IGrammar Members
+
+        public void Execute(IStep containerStep, ITestContext context)
+        {
+            StoryTellerAssert.Fail("I don't want to run");
+        }
+
+        public GrammarStructure ToStructure(FixtureLibrary library)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Description { get { throw new NotImplementedException(); } }
+
+        #endregion
+    }
+
+}
